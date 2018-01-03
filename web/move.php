7d1a4943325a8585eb5e8b9a44d5f170de70dce3
John Wiggins <jcwiggi@gmail.com>
2018-01-03 12:45:48 +0300

    made move.php give better descriptions of errors

--- web/move.php
+++ web/move.php
@@ -3,12 +3,19 @@ if (isset($_POST['dir'])) {
     $dir = $_POST['dir'];
     // todo
     // add stuff that makes rc car run and stuff
-    //$rd = ($dir == "forward" ? 0 : ($dir == "backward" ? 1 : ($dir == "left" ? 2 : ($dir == "right" ? 3 : 4))));
+    $rd = ($dir == 0 ? "forward" : ($dir == 1 ? "backward" : ($dir == 2 ? "right" : ($dir == 3 ? "left" : "stop"))));
     //$output = shell_exec("/usr/local/bin/rc_web $rd");
-    $output = shell_exec("/usr/local/bin/rc_web $dir &");
-    //if ($output) { echo "<pre>\ndir: [$dir]\n[$output]\n</pre>\n"; }
-    //else { echo "[$dir]"; }
-    echo "[$dir]";
+    $gp = shell_exec("gpio read 1");
+    if ($gp == 1) {
+      echo "[$rd";
+      $output = shell_exec("/usr/local/bin/rc_web $dir");
+      if ($output) {
+          echo " - $output";
+      }
+      echo "]";
+    } else {
+        echo "[no power]";
+    }
 } else {
     echo "[invalid command]";
 }
